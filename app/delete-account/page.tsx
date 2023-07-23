e45b1006ad43c79da940f1508f5f538777b1c85c
unknown <odebisiidowu09@gmail.com>
2023-07-24 00:32:04 +0100

    addeed file

--- app/delete-account/page.tsx
+++ app/delete-account/page.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import MarketPlaceNavBar from "../marketplace/MarketPlaceNavBar";
-import AcctLeft from "../user-account-home/AcctLeft";
 import AcctDeleteRight from "./AcctDeleteRight";
+import ManageAcctLeft from "../profile-details/ManageAcctLeft";
 
 function page() {
   return (
     <div className="bg-siteBG">
       <MarketPlaceNavBar />
       <div className="flex-col 804xl:flex-row px-3 804xl:px-16 pt-8 pb-16 flex gap-4">
-        <AcctLeft activeTab="" />
+        <ManageAcctLeft activeTab="delete-account" />
         <AcctDeleteRight />
       </div>
     </div>
