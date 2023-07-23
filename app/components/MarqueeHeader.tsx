abef495795e21ee454532df7ace33b2610e1a672
unknown <odebisiidowu09@gmail.com>
2023-07-24 00:46:12 +0100

    remove default scrollbar

--- app/components/MarqueeHeader.tsx
+++ app/components/MarqueeHeader.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
+import './marquee.css'
 import React from "react";
 import { CustomImageContain } from "../product-detail/ProdLeftContainer";
 
@@ -19,7 +20,7 @@ function MarqueeHeader() {
     <div className="py-10 px-0 md:px-28">
       <p className="text-center font-medium">Our Partners</p>
       {/* <div className="flex mt-5 md:justify-between"> */}
-      <div className="flex mt-5 overflow-x-scroll">
+      <div className="flex mt-5 overflow-x-scroll test">
         {partnerImages.map((item) => (
           <>
           <MarqueeHeaderItems key={item} image={item} />
